8adcc7b15605bf38431760982e145fc40ea7ff20
rqphy <raphael.ferreira@hetic.net>
2020-12-18 13:59:54 +0100

    create ArticleCard component

---
+++ src/Components/ArticleCard/ArticleCard.js
@@ -0,0 +1,21 @@
+import './ArticleCard.scss';
+
+function ArticleCard() {
+    return (
+        <article className="ArticleCard" >
+            <aside className="ArticleCard__aside" >
+                <p className="ArticleCard__category" >Frameworks</p>
+                <div className="ArticleCard__img" ></div>
+            </aside>
+            <main className="ArticleCard__main" >
+                <h3>Apprendre les bases du HTML et le JavaScript tranquillou</h3>
+                <div className="ArticleCard__author" >
+                    <div className="ArticleCard__pp"></div>
+                    <p className="ArticleCard__authorName">Berlingo</p>
+                </div>
+            </main>
+        </article>
+    )
+}
+
+export default ArticleCard;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
+import ArticleCard from "../../Components/ArticleCard/ArticleCard";
 import "./Home.scss";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <ArticleCard />
     </div>
   );
 }
