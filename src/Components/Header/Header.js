d0915caece1e27d3b0acc8fa96329865304c645f
rqphy <raphael.ferreira@hetic.net>
2020-12-19 16:55:26 +0100

    set active link to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,44 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <header className="Header" >
-            <div className="Header__logo" >Logo</div>
-            <nav className="Header__nav" >
-                <Link className="Header__link" to="/" >Home</Link>
-                <Link className="Header__link" to="/bases" >Bases</Link>
-                <Link className="Header__link" to="/frameworks" >Frameworks</Link>
-                <Link className="Header__link" to="/projects" >Projects</Link>
-                <Link className="Header__link" to="/git" >Git</Link>
-            </nav>
-            <a className="Header__about" href="#about" >À propos</a>
-        </header>
-    )
+  const currentPath = window.location.href.substring(21);
+  const links = [
+    { title: "Acceuil", url: "/", active: currentPath === "/" },
+    { title: "Bases", url: "/bases", active: currentPath === "/bases" },
+    {
+      title: "Frameworks",
+      url: "/frameworks",
+      active: currentPath === "/frameworks",
+    },
+    {
+      title: "Projects",
+      url: "/projects",
+      active: currentPath === "/projects",
+    },
+    { title: "Git", url: "/git", active: currentPath === "/git" },
+  ];
+
+  return (
+    <header className="Header">
+      <div className="Header__logo">Logo</div>
+      <nav className="Header__nav">
+        {links.map((link, index) => (
+          <Link
+            className={
+              link.active ? "Header__link Header__link--active" : "Header__link"
+            }
+            to={link.url}
+            key={index}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </nav>
+      <a className="Header__about" href="#about">
+        À propos
+      </a>
+    </header>
+  );
 }
 
 export default Header;
