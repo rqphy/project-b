856f35a7b232638e3756b16eff07ac0a3d902640
Marco <56915570+marcodmrn@users.noreply.github.com>
2020-12-20 20:30:26 +0100

    add navbar on mobile with img

--- src/Pages/Bases/Bases.js
+++ src/Pages/Bases/Bases.js
@@ -1,6 +1,11 @@
 function Bases() {
   return (
     <div className="Bases">
+      <img
+        class="Bases__image"
+        src="../../assets/navbar/bases.png"
+        alt="bases"
+      ></img>
       <h1 className="Bases__title">Bases</h1>
     </div>
   );
--- src/Pages/Frameworks/Frameworks.js
+++ src/Pages/Frameworks/Frameworks.js
@@ -1,6 +1,11 @@
 function Framework() {
   return (
     <div className="Frameworks">
+      <img
+        class="Frameworks__image"
+        src="../../assets/navbar/frameworks.png"
+        alt="Frameworks"
+      ></img>
       <h1 className="Frameworks__title">Frameworks</h1>
     </div>
   );
