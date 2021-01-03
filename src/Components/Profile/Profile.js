93ce489a97013484196bb84c10b824e85a923a7d
rqphy <raphael.ferreira@hetic.net>
2021-01-03 11:00:47 +0100

    add style

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,8 +1,9 @@
 function Profile() {
   return (
-    <div className="Profile">
-      <h5>Profile</h5>
-    </div>
+    <a className="Profile">
+      <div className="Profile__img"></div>
+      <h4 className="Profile__title">Profile</h4>
+    </a>
   );
 }
 
