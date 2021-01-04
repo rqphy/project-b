59974683284938aecba6859c9f62e961130aa4ce
Raphael <raphael.ferreira@hetic.net>
2021-01-04 21:38:19 +0100

    add smooth scroll and img max width

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -31,6 +31,66 @@ function Home() {
           </p>
         </article>
       </secion>
+      <secion className="about">
+        <h2 className="about__title">À propos</h2>
+        <article className="about__container">
+          <div className="about__team">
+            <Profile />
+            <Profile />
+            <Profile />
+            <Profile />
+          </div>
+          <p className="about__text">
+            Nous sommes un groupe de développeur web étudiant à Hetic. Vous
+            pouvez trouver plus d’info sur nous ci-dessus.
+          </p>
+          <p className="about__text">
+            Ce blog existe pour que vous puissiez retrouver plein d’infos sur le
+            developpement web! Si vous avez des questions ou des conseils
+            contactez nous.
+          </p>
+        </article>
+      </secion>
+      <secion className="about">
+        <h2 className="about__title">À propos</h2>
+        <article className="about__container">
+          <div className="about__team">
+            <Profile />
+            <Profile />
+            <Profile />
+            <Profile />
+          </div>
+          <p className="about__text">
+            Nous sommes un groupe de développeur web étudiant à Hetic. Vous
+            pouvez trouver plus d’info sur nous ci-dessus.
+          </p>
+          <p className="about__text">
+            Ce blog existe pour que vous puissiez retrouver plein d’infos sur le
+            developpement web! Si vous avez des questions ou des conseils
+            contactez nous.
+          </p>
+        </article>
+      </secion>
+      <secion className="about">
+        <h2 className="about__title">À propos</h2>
+        <article className="about__container">
+          <div className="about__team">
+            <Profile />
+            <Profile />
+            <Profile />
+            <Profile />
+          </div>
+          <p className="about__text">
+            Nous sommes un groupe de développeur web étudiant à Hetic. Vous
+            pouvez trouver plus d’info sur nous ci-dessus.
+          </p>
+          <p className="about__text">
+            Ce blog existe pour que vous puissiez retrouver plein d’infos sur le
+            developpement web! Si vous avez des questions ou des conseils
+            contactez nous.
+          </p>
+        </article>
+      </secion>
     </main>
   );
 }
